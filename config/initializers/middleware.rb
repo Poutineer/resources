b8896942143378037e8876b5079ff4298bc90d1f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-08-12 02:20:44 -0700

    Ensure the cookie expires

--- config/initializers/middleware.rb
+++ config/initializers/middleware.rb
@@ -1,6 +1,6 @@
 Poutineer::Application.config.middleware.tap do |middleware|
   middleware.insert_before(Warden::Manager, ActionDispatch::Cookies)
-  middleware.insert_before(Warden::Manager, ActionDispatch::Session::CookieStore, :key => "_poutineer_session")
+  middleware.insert_before(Warden::Manager, ActionDispatch::Session::CookieStore, :key => "_poutineer_session", :expire_after => 14.days)
   middleware.insert_before(Warden::Manager, Rack::AuthenticationBearer, &Base64.method(:urlsafe_decode64))
   middleware.use(Rack::Deflater)
   middleware.use(Rack::Attack)
