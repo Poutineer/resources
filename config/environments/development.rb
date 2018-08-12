327440b8c0919a61de550ac12ca57136856c82b8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-08-12 00:38:24 -0700

    More active storage configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,10 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
   end
 
-  # Don"t care if the mailer can"t send.
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
+
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
