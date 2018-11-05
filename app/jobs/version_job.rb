9909476a3a11590db72758f3b4b4aa6446298832
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-11-05 15:52:04 -0800

    Getting ready for the upgrade

--- app/jobs/version_job.rb
+++ app/jobs/version_job.rb
@@ -1,7 +1,18 @@
 class VersionJob < ApplicationJob
   sidekiq_options(:queue => "versions", :unique_across_queues => true, :lock => :until_executed, :log_duplicate_payload => true)
 
-  def perform(version_class, attributes)
-    version_class.constantize.create!(attributes)
+  def perform(version_class, attributes, event)
+    version = version_class.constantize.create!(attributes)
+
+    unless event == :create && version.errors.any?
+      log_version_errors(version, event)
+    end
+  end
+
+  def log_version_errors(version, action)
+    version.logger&.warn(
+      "Unable to create version for #{action} of #{version.item.class.name}" \
+        "##{version.item.id}: " + version.errors.full_messages.join(", ")
+    )
   end
 end
